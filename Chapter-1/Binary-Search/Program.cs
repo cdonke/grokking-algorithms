7b95dcb021b505778ca295173cf2ab01467f41e0
Christiano Donke <cdonke@MacBook-Air-de-Christiano.local>
2018-07-08 20:29:30 -0300

    Chapter 1 - Binary Search

---
+++ Chapter-1/Binary-Search/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Binary_Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myList = new[] { 1, 3, 5, 7, 9 };
+
+            Console.WriteLine($"Index of 3 is {binary_search(myList, 3)}");
+
+            Console.WriteLine($"Index of 2 is {binary_search(myList, 2)}");
+
+        }
+
+
+        static int binary_search(int[] list, int item)
+        {
+            int low = 0;
+            int high = list.Length - 1;
+            int mid, guess;
+
+            while (low <= high)
+            {
+                mid = (low + high) / 2;
+                guess = list[mid];
+
+                if (guess == item)
+                    return mid;
+                else if (guess > item)
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
+            return -1;
+        }
+    }
+}
