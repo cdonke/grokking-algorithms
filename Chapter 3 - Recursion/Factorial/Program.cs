a504f1afec30947a5f9b4e33fbe53fb8d36f3e6c
Christiano Donke <cdonke@MacBook-Air-de-Christiano.local>
2018-07-08 21:03:36 -0300

    Chapter 3 - Recursion

---
+++ Chapter 3 - Recursion/Factorial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"5!={Factorial(5)}");
+        }
+
+        static int Factorial(int x)
+        {
+            if (x == 1)
+                return 1;
+            else
+                return x * Factorial(x - 1);
+        }
+    }
+}
